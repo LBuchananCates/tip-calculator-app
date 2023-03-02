fcbfacacb6d2670f538c71b348a8cf1d2a7721ee
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-03-01 20:12:09 -0500

    first js commit

---
+++ tip-calculator.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+const fivePercent = document.querySelector(".five");
+const tenPercent = document.querySelector(".ten");
+const fifteenPercent = document.querySelector(".fifteen");
+const twentyFivePercent = document.querySelector(".twenty-five");
+const fiftyPercent = document.querySelector(".fifty");
+
+// total per person = total bill-input.value + (bill-input.value x tip%) divided by num-people-input.value
+
+// tip amount = bill-input x percentage selected
+
+// reset button clears all input values or refreshes page
